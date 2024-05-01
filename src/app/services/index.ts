f9eed82cb2756b12c54d3fd4fcb3d03cdb62b12d
junex10 <43711689+junex10@users.noreply.github.com>
2024-05-01 15:49:09 -0400

    Adding removing and recipe service

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -5,11 +5,12 @@ import { PatientSocketsService } from './sockets/patient/sockets.service';
 import { LoginService } from './login/login.service';
 import { AuthService } from './auth/auth.service';
 import { ProfileService } from './profile/profile.service';
-
+import { RecipeService } from './recipes/recipes.service';
 
 export {
     PatientSocketsService,
     LoginService,
     AuthService,
-    ProfileService
+    ProfileService,
+    RecipeService
 }
--- src/app/web/pages/dashboard/recipes/recipes.component.ts
+++ src/app/web/pages/dashboard/recipes/recipes.component.ts
@@ -3,7 +3,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
-import { Constants } from 'src/app/shared';
+import { RecipeService, AuthService } from 'src/app/services';
+import Swal from 'sweetalert2';
+import {
+  Constants,
+  SwalAlerts
+} from 'src/app/shared';
 
 @Component({
   selector: 'app-recipes',
@@ -18,6 +23,7 @@ export class RecipesComponent implements OnInit {
   itemSelected: any = {};
   animal: string = '';
   recipeImage: string = 'assets/img/bg-img/bg4.jpg';
+  user: any;
 
   form: FormGroup;
 
@@ -87,6 +93,8 @@ export class RecipesComponent implements OnInit {
   constructor(
     public dialog: MatDialog,
     private fb: FormBuilder,
+    private auth: AuthService,
+    private recipe: RecipeService
   ) {
     this.form = this.fb.group({
       name: [null, Validators.required],
@@ -101,13 +109,38 @@ export class RecipesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
+    this.user = this.auth.getUser()?.user;
   }
 
   openRecipes = () => this.openEditModal = true;
 
   newRecipe = () => {
-    console.log(this.form.value, ' HERE ')
+    if (this.form.invalid) {
+      Swal.fire(SwalAlerts.swalCustom(
+        `<div>
+          <h4 class='text-center'>Invalid form</h4>
+          <p style='font-size: 15px;' class='mt-4'>There's some error in the form, please check before to send</p>
+        </div>`,
+        {
+          showCancelButton: false,
+          showConfirmButton: false,
+          timer: 3000,
+          icon: 'error'
+        }
+      ));
+      return;
+    } else {
+      this.recipe.newRecipe({
+        ...this.form.value,
+        user_id: this.user.id,
+        formData: true
+      })
+      .then(() => {
+        Swal.fire(SwalAlerts.swalSuccess('Recipe', 'Recipe created'));
+        this.openEditModal = false;
+      });
+    }
+    
   }
 
   onImage = (file: any) => {
